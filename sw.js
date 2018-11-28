12e83e984a26f6120a501729747d4274f1e0b4fc
Victor Ribeiro <victorqribeiro@gmail.com>
2018-11-28 04:21:41 -0200

    fix service worker

--- sw.js
+++ sw.js
@@ -1,7 +1,31 @@
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/favicon.png',
+  '/manifest.json',
+  '/css/main.css',
+  '/js/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      }).catch(reason => reason)
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    ).catch(reason => reason)
   );
 });
